90be07c760da309f03651a03f81220399d750012
Stevenhr <estevenhr@hotmail.com>
2024-11-03 10:47:01 -0500

    Feat(Table):Stylos tabla y datos modelo

--- src/app/cafes/coffees-table/coffees-table.component.ts
+++ src/app/cafes/coffees-table/coffees-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cafe } from '../Cafe';
+import { CafeService } from '../cafe.service';
 @Component({
   selector: 'app-coffees-table',
   templateUrl: './coffees-table.component.html',
@@ -7,9 +8,24 @@ import { Cafe } from '../Cafe';
 })
 export class CoffeesTableComponent implements OnInit {
   cafes: Array<Cafe> = []; 
-  constructor() { }
+  constructor(private cafeService: CafeService) { }
+
+  getCafes(): void {
+    this.cafeService.getCafes().subscribe((cafes) => {
+      this.cafes = cafes;
+    });
+  }
+
+  get totalOrigen() {
+    return this.cafes.filter(cafe => cafe.tipo === 'Café de Origen').length;
+  }
+
+  get totalBlend() {
+    return this.cafes.filter(cafe => cafe.tipo === 'Blend').length;
+  }
 
   ngOnInit() {
+    this.getCafes();
   }
 
 }
