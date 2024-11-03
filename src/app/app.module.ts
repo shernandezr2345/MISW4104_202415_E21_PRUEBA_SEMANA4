bed283d15e1137a4527f9ae6461c15c351041600
Stevenhr <estevenhr@hotmail.com>
2024-11-03 00:20:39 -0500

    feat(Cafe): Header vista variedades cafe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CafesModule } from './cafes/cafes.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CafesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
