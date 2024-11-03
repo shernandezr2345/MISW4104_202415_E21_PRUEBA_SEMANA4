1e8edc0c5afa2ac5b58a693bf561dc09dd8c720d
Stevenhr <estevenhr@hotmail.com>
2024-11-03 06:46:25 -0500

    feat(table): Inicio estructura table titles

--- src/app/cafes/cafes.module.ts
+++ src/app/cafes/cafes.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { CoffeesTableComponent } from './coffees-table/coffees-table.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  exports: [HeaderComponent],
-  declarations: [HeaderComponent]
+  exports: [HeaderComponent,CoffeesTableComponent],
+  declarations: [HeaderComponent,CoffeesTableComponent]
 })
 export class CafesModule { }
---
+++ src/app/cafes/coffees-table/coffees-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coffees-table',
+  templateUrl: './coffees-table.component.html',
+  styleUrls: ['./coffees-table.component.css']
+})
+export class CoffeesTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
